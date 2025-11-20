f1bf4cb2d25cb3ff22ee8aae59d59d47d9f0915d
Rohit Mhatre <rohitmhatre578@gmail.com>
2025-11-20 15:02:44 +0530

    Initial commit - added python practice files

---
+++ chapter -1/problem3.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("hey hii rohit how are you")
+engine.runAndWait()
---
+++ chapter -1/problrm4.py
@@ -0,0 +1,8 @@
+import os
+
+directory_path = '/'
+
+contents = os.listdir(directory_path)
+
+for i in contents:
+    print(i)
